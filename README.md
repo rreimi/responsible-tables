6957b1ca5192b4f0f94f947d64491ec6d7b35f5b
Robert Reimi <robert.reimi@gmail.com>
2013-09-08 03:13:19 -0430

    Updated plugin code, Added minified versions, Added demo page, Updated readme.md

--- README.md
+++ README.md
@@ -1,4 +1,87 @@
 magic-tables
 ============
 
-jQuery responsive tables plugin
+jQuery responsive tables plugin its a small plugin that allows you to give a responsive behavior for your tables.
+
+
+Features
+--------
+
+- Make your tables responsive easily
+- Select columns/headings you want to show or not
+- Set a custom heading for rows in responsive mode based on templates
+- Control responsive behavior based on viewport width
+- Based on the Chirs Coyier responsive tables approach described <a href="http://css-tricks.com/responsive-data-tables/">here</a>
+- Lightweigth plugin ~< 2kb
+
+Usage
+-----
+
+
+- jQuery is required
+- Download the plugin <a href="">magic-tables.min.js</a> js file
+- Download the plugin <a href="">magic-tables.min.css</a> css file
+
+
+Include both files in your project and simply call the magicTable plugin over a table:
+
+`````javascript
+
+<script type="text/javascript">
+    jQuery('.mytable').magicTable();
+</script>
+
+`````
+
+Options
+-------
+
+You can pass a diferent options to customize responsive behavior
+
+- <b>responsiveWidth</b> <i>default: 1024</i>
+
+Set the min viewport width that enables responsive behavior.
+
+- <b>skipColumns</b> <i>default: []</i>
+
+Array with columns index you want to skip in responsive mode, starting from 0.
+
+i.e. if you want to hide the first and third column you have to pass [0,2]
+
+- <b>displayLabels</b> <i>default: true</i>
+
+Controls column heading display for all rows
+
+- <b>skipLabels</b> <i>default: []</i>
+
+Array with columns heads you want to skip in responsive mode, starting from 0.
+
+- <b>customTitle</b> <i>default: ''</i>
+
+A html template to create custom heading for each row.
+
+If is set, a new column will be added and the beginning of each row using the specfic template. 
+
+You can define simple templates like
+
+`````html
+<b>{0}</b>. 
+`````
+
+{0} will be replace for the column 0 value. 
+
+You can reference any column, no matter if that column are in the skipColumns array. 
+In fact, you may want to use this feature in combination with skipColumns to create custom heading for each row in responsive mode and hide referenced columns.
+
+See <a href="http://magictables.androb.com/#custom-title">custom title example</a> in demo page to give a better idea. 
+
+
+
+- <b>labelSeparator</b> <i>default: ':'</i>
+
+Set separator between column heading and column value in responsive mode.
+
+Demo
+----
+
+See some demos <a href="http://magictables.androb.com">here</a>
--- js/magic-tables.js
+++ js/magic-tables.js
@@ -2,7 +2,7 @@
     $.fn.magicTable = function(opts){
 
         var options = $.extend({}, {
-            skipFields: [],
+            skipColumns: [],
             displayLabels: true,
             skipLabels: [],
             customTitle: '',
@@ -36,28 +36,43 @@
                 header[i] = table.rows[0].cells[i];
             }
 
+            jQuery(table.rows[0]).addClass(' rt-skip');
+
+            var index = 1;
             for(var i = 1; i < table.rows.length; i++){
 
                 //Skip tfoot & thead
                 if ((table.rows[i].parentNode.nodeName === 'THEAD') || (table.rows[i].parentNode.nodeName === 'TFOOT')){
+                    jQuery(table.rows[i]).addClass(' rt-skip')
                     continue;
                 }
+
+                if ((index) % 2 == 0){
+                    jQuery(table.rows[i]).addClass('rt-odd');
+                } else {
+                    jQuery(table.rows[i]).addClass('rt-even');
+                }
+
+                index++;
 
                 var theTitle = options.customTitle;
                 for (var j = 0; j < table.rows[i].cells.length; j++){
 
                     var className = table.rows[i].cells[j].className;
 
-                    if (options.skipFields.indexOf(j) >= 0){
+                    if (options.skipColumns.indexOf(j) >= 0){
                         className += " rt-skip rt-row rt-row-" + j;
                     }
 
                     //Build the title
                     if (theTitle != '') {
-                        theTitle = theTitle.replace('{' + j + '}', table.rows[i].cells[j].innerHTML);
+                        theTitle = theTitle.replace(new RegExp('\\{' + j + '}',"g"), table.rows[i].cells[j].innerHTML);
                     }
 
-                    table.rows[i].cells[j].className = className;
+                    if (className != ''){
+                        table.rows[i].cells[j].className = className;
+                    }
+
                     var rowContent = '';
 
                     if (options.displayLabels) {
@@ -70,10 +85,12 @@
                     table.rows[i].cells[j].innerHTML = rowContent;
                 }
 
-                var theKid = document.createElement("td");
-                theKid.innerHTML = theTitle;
-                theKid.className = 'rt-visible';
-                table.rows[i].insertBefore(theKid, table.rows[i].firstChild);
+                if (theTitle != '') {
+                    var theKid = document.createElement("td");
+                    theKid.innerHTML = theTitle;
+                    theKid.className = 'rt-block';
+                    table.rows[i].insertBefore(theKid, table.rows[i].firstChild);
+                }
             }
         });
 
@@ -81,11 +98,11 @@
         jQuery(window).on("resize", function(){
             jQuery('.magic-table').each(function (){
                 var isResponsive = jQuery(this).hasClass('rt-table');
-                if ((viewport().width < options.responsiveWidth) && (!isResponsive)){
+                if ((viewport().width <= options.responsiveWidth) && (!isResponsive)){
                     jQuery(this).addClass('rt-table');
                 }
 
-                if ((viewport().width >= options.responsiveWidth) && (isResponsive)) {
+                if ((viewport().width > options.responsiveWidth) && (isResponsive)) {
                     jQuery(this).removeClass('rt-table');
                     //console.log('change table to not responsive');
                 }
